48164e56df4f3402e07ac9ce5ec8c328a2328205
Viktoriia <babenko_v_v@ukr.net>
2016-11-11 13:23:51 +0200

    upd

--- build/js/common.js
+++ build/js/common.js
@@ -100,6 +100,74 @@ console.log($('.js-html-slides'));
 		ClosePopup(popup);
 	});
 
-// =include all-fun.js
+function HideShow(allBl, blActive, allTab, tabActive) {
+
+		allTab.removeClass('is-active');
+
+		allBl.hide();
+
+		tabActive.addClass('is-active');
+
+		blActive.fadeIn();
+
+};
+
+
+
+function OnSlick(slider_bl, toShow,i){
+
+	if (!(slider_bl.hasClass('slick-initialized'))) {
+
+
+
+				slider_bl.slick({
+
+					slidesToShow: toShow,
+
+					slidesToScroll: 1,
+
+					arrows: true,
+
+					dots: false,
+
+					autoplay: false,
+
+					infinite: false,
+
+					initialSlide: i
+
+				});
+
+	}
+
+};
+
+
+
+function OffScroll(){
+
+	var winScrollTop = $(window).scrollTop();
+
+
+
+	$(window).on('scroll',function () {
+
+		$(window).scrollTop(winScrollTop);
+
+	});
+
+};
+
+
+
+function ClosePopup(popup_bl){
+
+	popup_bl.removeClass('is-open');
+
+	$(window).unbind('scroll'); //Выключить отмену прокрутки
+
+};
+
+
 
 });
